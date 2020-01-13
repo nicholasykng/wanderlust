8682297e573e740c6508e8ce3358fa08af64e2ba
Nicholas Ng <nicholasykng@gmail.com>
2020-01-13 04:44:38 +0000

    tweak migrations

--- db/migrate/20200113042552_create_attractions.rb
+++ db/migrate/20200113042552_create_attractions.rb
@@ -1,4 +1,9 @@
 class CreateAttractions < ActiveRecord::Migration[6.0]
   def change
+    create_table :attractions do |t|
+      t.text :site
+      t.text :comments
+      t.integer :destination_id
+    end
   end
 end
--- db/migrate/20200113042604_create_destinations.rb
+++ db/migrate/20200113042604_create_destinations.rb
@@ -1,4 +1,9 @@
 class CreateDestinations < ActiveRecord::Migration[6.0]
   def change
+    create_table :destinations do |t|
+      t.text :country
+      t.text :city
+      t.integer :user_id
+    end
   end
 end
