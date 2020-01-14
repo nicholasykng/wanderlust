6deee3a1007e89ef9efbb5c79eb656cbad56d843
Nicholas Ng <nicholasykng@gmail.com>
2020-01-14 03:44:47 +0000

    login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,32 @@ class UsersController < ApplicationController
   end
 
   post "/signups" do
-    
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to "/signups"
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/"
+    end
+  end
+
+  get "/login" do
+    erb :"/users/login"
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect to "/login"
   end
 end
