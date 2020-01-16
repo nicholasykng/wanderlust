d0bc083667f6a7f3cd68dfa5314d8e5340b94416
Nicholas Ng <nicholasykng@gmail.com>
2020-01-16 04:40:32 +0000

    update attractions controller

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -18,6 +18,53 @@ class AttractionsController < ApplicationController
         @destination.attractions << @attraction
         redirect to "/destinations/#{@destination.id}"
       end
+    else
+      redirect to "/login"
+    end
+  end
+
+  get "/destinations/:destination_id/:attraction_id/edit" do
+    if logged_in?
+      @destination = Destination.find_by(id: params[:destination_id])
+      @attraction = Attraction.find_by(id: params[:attraction_id])
+      if @destination && @attraction && @destination.user == current_user && @attraction.destination_id == @destination.id
+        erb :"/attractions/edit"
+      else
+        redirect to "/destinations"
+      end
+    else
+      redirect to "/login"
+    end
+  end
+
+  patch "/destinations/:destination_id/:attraction_id" do
+    if logged_in?
+      @destination = Destination.find_by(id: params[:destination_id])
+      @attraction = Attraction.find_by(id: params[:attraction_id])
+      if params[:site] == "" || params[:comments] == ""
+        redirect to "/destinations/<%= @destination.id %>/<%= @attraction.id %>"
+      else
+        @attraction.site = params[:site]
+        @attraction.comments = params[:comments]
+        @attraction.save
+        redirect to "/destinations/#{@destination.id}"
+      end
+    else
+      redirect to "/login"
+    end
+  end
+  delete "/destinations/:destination_id/:attraction_id" do
+    if logged_in?
+      @destination = Destination.find_by(id: params[:destination_id])
+      @attraction = Attraction.find_by(id: params[:attraction_id])
+      if @destination && @attraction && @attraction.user == current_user
+        @attraction.delete
+        redirect to "/destinations/#{@destination.id}"
+      else
+        redirect to "/destinations"
+      end
+    else
+      redirect to "/login"
     end
   end
 end
