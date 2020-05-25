154a0f415b85025eabdee963248168ee38654e26
nicholasykng <nicholasykng@gmail.com>
2020-05-24 20:25:21 -0700

    added changes to postgresql

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
-
+#ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
+set :database_file, "./database.yml"
 require_all 'app'
