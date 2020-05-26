8be3e3ee119541ac1f24dca9b96c584d6cc39d72
nicholasykng <nicholasykng@gmail.com>
2020-05-26 12:55:15 -0700

    public folder issues

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
 #ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 set :database_file, "./database.yml"
 require_all 'app'
+require_all "public"
