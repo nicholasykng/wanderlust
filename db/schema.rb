bd019e7a0846aedc7bf4350d9818f5c61eb56bda
nicholasykng <nicholasykng@gmail.com>
2020-05-26 11:07:27 -0700

    fixed css issues

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_13_042604) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attractions", force: :cascade do |t|
     t.text "site"
     t.text "comments"
